1915e7550161b6e2696511b76852bdc3d3c26350
nirbenator <nir.benatar@komodor.io>
2023-06-20 11:03:12 +0300

    chore: sync instead of helm s3 push

--- publish.sh
+++ publish.sh
@@ -35,36 +35,38 @@ find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; d
   chart_name="`basename "$chart"`"
   echo ">>> fetching chart $chart_name version"
   chart_version=$(cat $chart/Chart.yaml | grep -oE "version:\s[0-9]+\.[0-9]+\.[0-9]+" | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
-  echo ">>> checking if version is already published"
-  if [ -f "$chart_name/$chart_name-$chart_version.tgz" ]; then
-    echo ">>> VERSION $chart_version ALREADY EXISTS! Skipping..."
-    continue
-  else
-    echo ">>> chart version is valid, continuing..."
-  fi
+  # echo ">>> checking if version is already published"
+  # if [ -f "$chart_name/$chart_name-$chart_version.tgz" ]; then
+  #   echo ">>> VERSION $chart_version ALREADY EXISTS! Skipping..."
+  #   continue
+  # else
+  #   echo ">>> chart version is valid, continuing..."
+  # fi
   echo ">>> helm lint $chart"
   helm lint "$chart"
   echo ">>> helm package -d $chart_name $chart"
   mkdir -p "$chart_name"
   helm package -d "$chart_name" "$chart"
-  helm repo add komodor-helm-charts-com s3://helm-charts.komodor.com
-  helm repo add komodor-helm-charts-io s3://helm-charts.komodor.io
-  helm s3 push $chart_name/$chart_name-$chart_version.tgz komodor-helm-charts-io --relative
-  helm s3 push $chart_name/$chart_name-$chart_version.tgz komodor-helm-charts-com --relative
+  aws s3 sync "$chart_name" s3://helm-charts.komodor.com/"$chart_name"
+  aws s3 sync "$chart_name" s3://helm-charts.komodor.io/"$chart_name"
 done
 echo '>>> helm repo index'
 helm repo index .
 
-if [ "$BUILDKITE_BRANCH" != "master" ]; then
-  echo "Current branch is not master and do not publish"
-  exit 0
-fi
+echo '>>> Syncing to S3'
+aws s3 cp index.yaml s3://helm-charts.komodor.com/index.yaml
+aws s3 cp index.yaml s3://helm-charts.komodor.io/index.yaml
 
-echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
-git config user.email "buildkite@users.noreply.github.com"
-git config user.name Buildkite
-git add .
-git status
-git commit -m "Published by Buildkite $BUILDKITE_BUILD_URL"
-git push origin "$GITHUB_PAGES_BRANCH"
+# if [ "$BUILDKITE_BRANCH" != "master" ]; then
+#   echo "Current branch is not master and do not publish"
+#   exit 0
+# fi
+
+# echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
+# git config user.email "buildkite@users.noreply.github.com"
+# git config user.name Buildkite
+# git add .
+# git status
+# git commit -m "Published by Buildkite $BUILDKITE_BUILD_URL"
+# git push origin "$GITHUB_PAGES_BRANCH"
 
