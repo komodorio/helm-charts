9b1c280e67b5ddef56042b72fd436f50426b3d50
MoranKa <moran@komodor.io>
2023-10-15 10:15:37 +0300

    Combine two tests into one add retry to pod ready check due to change in network-mapper readiness

--- .buildkite/tests/basic_test.py
+++ .buildkite/tests/basic_test.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from fixtures import setup_cluster, kube_client, cleanup_agent_from_cluster
 from helpers.utils import get_filename_as_cluster_name
 import helpers.kubernetes_helper as kubernetes_helper
@@ -23,17 +24,22 @@ def test_dont_provide_required_values(setup_cluster, settings, missing_value):
     assert exit_code != 0, f"helm install should fail, output: {output}"
 
 
-def test_helm_installation(setup_cluster):
+def test_helm_installation(setup_cluster, kube_client):
     output, exit_code = helm_agent_install(CLUSTER_NAME)
     assert exit_code == 0, "helm install failed, output: {}".format(output)
 
-
-def test_all_pods_are_running_in_chart(setup_cluster, kube_client):
-    output, exit_code = helm_agent_install(CLUSTER_NAME)
-    assert exit_code == 0, "helm install failed, output: {}".format(output)
-
-    kubernetes_helper.check_pods_running(kube_client, 'app.kubernetes.io/name=komodor-agent')
-    kubernetes_helper.check_pods_running(kube_client, 'app.kubernetes.io/name=komodor-agent-daemon')
+    last_exception = None
+    for _ in range(10):
+        try:
+            kubernetes_helper.check_pods_running(kube_client, 'app.kubernetes.io/name=komodor-agent')
+            kubernetes_helper.check_pods_running(kube_client, 'app.kubernetes.io/name=komodor-agent-daemon')
+            break
+        except AssertionError as e:
+            print("Waiting for pods to be ready")
+            last_exception = e
+            time.sleep(10)
+    else:
+        assert False, f"Pods are not ready: {last_exception}"
 
 
 def test_get_configmap_from_resources_api(setup_cluster):
