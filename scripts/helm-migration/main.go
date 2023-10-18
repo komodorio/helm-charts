9303c61759f1568c7e1a8edeefb4b1ede13aeda0
buildkite <buildkite@users.noreply.github.com>
2023-10-18 16:41:36 +0300

    fix helm migration wrong mapping Commit has to version

--- scripts/helm-migration/main.go
+++ scripts/helm-migration/main.go
@@ -18,6 +18,7 @@ type HelmRelease struct {
 }
 
 var version = "dev"
+var commit = "none"
 
 func main() {
 	var outputFile string
@@ -64,7 +65,7 @@ func printUpdateCommand(releaseName string, outputFile string, namespace string)
 
 func showVersionAndExit(showVersion bool) {
 	if showVersion {
-		fmt.Printf("Version: %s\n", version)
+		fmt.Printf("Version: %s, Commit: %s\n", version, commit)
 		os.Exit(0)
 	}
 }
