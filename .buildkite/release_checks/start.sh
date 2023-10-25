d40c2188cc87577b237b35708bc0aba89e5ed16a
MoranKa <moran@komodor.io>
2023-10-25 23:35:50 +0300

    updates for CI

--- .buildkite/release_checks/start.sh
+++ .buildkite/release_checks/start.sh
@@ -5,10 +5,11 @@ cd /app
 gcloud auth activate-service-account --key-file=sa.json
 
 ln -s sa.json gcp-tf/sa.json
+pushd gcp-tf
 terraform workspace new test || true
 terraform workspace select test
 terraform init
 terraform apply -var="cluster_name=test" -auto-approve
 terraform output kubeconfig > ../kubeconfig.yaml
-
+popd
 timeout --preserve-status 10m python3 /app/main.py kubeconfig.yaml
