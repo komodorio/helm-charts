928570fcb9996a5b114884c98b5c9c628eb0efe1
MoranKa <moran@komodor.io>
2023-10-26 00:22:12 +0300

    another small fix

--- .buildkite/release_checks/start.sh
+++ .buildkite/release_checks/start.sh
@@ -6,9 +6,9 @@ gcloud auth activate-service-account --key-file=sa.json
 
 cp sa.json gcp-tf/sa.json
 pushd gcp-tf
+terraform init
 terraform workspace new test || true
 terraform workspace select test
-terraform init
 terraform apply -var="cluster_name=test" -auto-approve
 terraform output kubeconfig > ../kubeconfig.yaml
 popd
