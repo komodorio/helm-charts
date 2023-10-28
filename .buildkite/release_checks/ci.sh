709ee6e5cdb6a3769c0fa60e4d62d8265d208016
MoranKa <moran@komodor.io>
2023-10-28 10:13:32 +0300

    Use parameter expansion

--- .buildkite/release_checks/ci.sh
+++ .buildkite/release_checks/ci.sh
@@ -5,12 +5,12 @@ SCRIPT_DIR=$(dirname $(realpath "$0"))
 pushd $SCRIPT_DIR
 
 run_in_docker() {
-  command="$1"
+
   docker run -it --rm \
     -v $(pwd):/app \
     -e RUN_TIMEOUT=${RUN_TIMEOUT:-"10m"} \
     634375685434.dkr.ecr.us-east-1.amazonaws.com/k8s-gcp-tools \
-    "${command}"
+    "$@"
 }
 
 work_mode=$(buildkite-agent meta-data get job-mode || echo "ga")
@@ -22,4 +22,4 @@ if [[ "$work_mode" != "ga" ]]; then
 fi
 
 echo $SA_KEY > sa.json
-run_in_docker "./start.sh $rc_tag"
+run_in_docker "./start.sh" "$rc_tag"
