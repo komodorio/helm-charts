d00bb96c1d5fa7ac8852677549a0492c0515c5ce
MoranKa <moran@komodor.io>
2023-10-30 13:27:49 +0200

    Change pipeline to support RC and GA versions

--- .buildkite/pipeline_scripts/bump_version.sh
+++ .buildkite/pipeline_scripts/bump_version.sh
@@ -1,19 +1,39 @@
 #!/usr/bin/env bash
 set -ex
 
-get_current_version() {
-    grep 'version:' charts/"${1}"/Chart.yaml | awk '{print $2}'
-}
-
-increment_version() {
-    echo "${1}" | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g'
-}
-
 configure_git() {
     git config user.email buildkite@users.noreply.github.com
     git config user.name buildkite
     git fetch --tags
     git checkout master
+}
+
+generate_next_version() {
+    local increment_type=$1
+    local tags=$(git tag -l 'komodor-agent/*' | awk -F'/' '{print $NF}')
+    local latest_tag=$(printf "%s\n" "${tags[@]}" | sort -V | tail -n 1 | tr '[:lower:]' '[:upper:]')
+
+    if [[ ${increment_type} == "rc" ]]; then
+        if [[ ${latest_tag} == *"+RC"* ]]; then
+            local base_version=${latest_tag%+RC*}
+            local rc_part=${latest_tag##*+RC}
+            local next_rc_number=$(( rc_part + 1 ))
+            echo "${base_version}+RC${next_rc_number}"
+        else
+            echo "${latest_tag}+RC1"
+        fi
+    else
+        local major=$(echo $latest_tag | awk -F'.' '{print $1}')
+        local minor=$(echo $latest_tag | awk -F'.' '{print $2}')
+        local patch=$(echo $latest_tag | awk -F'.' '{print $3}' | awk -F'+RC' '{print $1}')
+        if [[ ${increment_type} == "major" ]]; then
+            echo "$((major + 1)).0.0"
+        elif [[ ${increment_type} == "minor" ]]; then
+            echo "${major}.$((minor + 1)).0"
+        elif [[ ${increment_type} == "patch" ]]; then
+            echo "${major}.${minor}.$((patch + 1))"
+        fi
+    fi
 }
 
 get_app_version() {
@@ -26,7 +46,7 @@ get_app_version() {
      grep 'appVersion:' charts/$chart/Chart.yaml | awk '{print $2}'
 }
 
-update_chart_version() {
+update_chart_app_version() {
     local chart=$1
     local app_version=$2
 
@@ -34,14 +54,16 @@ update_chart_version() {
     sed -i -e "s/appVersion.*/appVersion: $app_version/g" charts/$chart/Chart.yaml
     buildkite-agent meta-data set "agent-version" "$app_version"
 
-    local current_version=$(get_current_version "${chart}")
-    local new_version=$(increment_version "$current_version")
-
-    echo "Updating chart '$chart' version from $current_version to $new_version"
-    sed -i -e "s/$current_version/$new_version/g" charts/$chart/Chart.yaml
     git add charts/$chart/Chart.yaml
-    buildkite-agent meta-data set "$chart-version" "$new_version"
 }
+
+increment_version() {
+  increment_type=$(buildkite-agent meta-data get "version-type" --job ${PARENT_JOB_ID} || echo "rc")
+  new_version=$(generate_next_version "$increment_type")
+  git tag "$new_version"
+  buildkite-agent meta-data set "$chart-version" "$new_version"
+}
+
 
 update_readme() {
     pushd charts/komodor-agent && make generate-readme && popd
@@ -49,8 +71,9 @@ update_readme() {
 }
 
 commit_and_push() {
-    git commit -m "[skip ci] increment chart versions" || echo "Already up-to-date"
+    git commit -m "[skip ci] update Chart.yaml" || echo "Already up-to-date"
     git push -f || echo "Nothing to push!"
+    git push --tags || echo "No tags to push"
 }
 
 ##################
@@ -58,10 +81,10 @@ commit_and_push() {
 ##################
 configure_git
 
-for chart in k8s-watcher komodor-agent; do
-    app_version=$(get_app_version $chart)
-    update_chart_version $chart $app_version
-done
+chart="komodor-agent"
+app_version=$(get_app_version "$chart")
+update_chart_app_version "$chart" "$app_version"
 
 update_readme
+increment_version
 commit_and_push
---
+++ .buildkite/pipeline_scripts/legacy_bump_version.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+set -ex
+
+get_current_version() {
+    grep 'version:' charts/"${1}"/Chart.yaml | awk '{print $2}'
+}
+
+increment_version() {
+    echo "${1}" | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g'
+}
+
+configure_git() {
+    git config user.email buildkite@users.noreply.github.com
+    git config user.name buildkite
+    git fetch --tags
+    git checkout master
+}
+
+get_app_version() {
+    local chart=$1
+    ver=$(buildkite-agent meta-data get "version" --job ${PARENT_JOB_ID})
+     if [ $? -eq 0 ]; then
+        echo $ver
+        return
+     fi
+     grep 'appVersion:' charts/$chart/Chart.yaml | awk '{print $2}'
+}
+
+update_chart_version() {
+    local chart=$1
+    local app_version=$2
+
+    echo "Updating app version to $app_version"
+    sed -i -e "s/appVersion.*/appVersion: $app_version/g" charts/$chart/Chart.yaml
+    buildkite-agent meta-data set "agent-version" "$app_version"
+
+    local current_version=$(get_current_version "${chart}")
+    local new_version=$(increment_version "$current_version")
+
+    echo "Updating chart '$chart' version from $current_version to $new_version"
+    sed -i -e "s/$current_version/$new_version/g" charts/$chart/Chart.yaml
+    git add charts/$chart/Chart.yaml
+    buildkite-agent meta-data set "$chart-version" "$new_version"
+}
+
+update_readme() {
+    pushd charts/komodor-agent && make generate-readme && popd
+    git add charts/komodor-agent/README.md || echo "Nothing to add"
+}
+
+commit_and_push() {
+    git commit -m "[skip ci] increment chart versions" || echo "Already up-to-date"
+    git push -f || echo "Nothing to push!"
+}
+
+##################
+# Main Execution #
+##################
+configure_git
+
+cart="k8s-watcher"
+
+app_version=$(get_app_version "$chart")
+update_chart_version "$chart" "$app_version"
+
+commit_and_push
