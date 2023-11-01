a722fd84581680c6f4a433c5ebe1adc11693282b
buildkite <buildkite@users.noreply.github.com>
2023-11-01 09:13:10 +0200

    Improve publish to only new chart when in GA mode

--- .buildkite/pipeline_scripts/publish_helm_charts.sh
+++ .buildkite/pipeline_scripts/publish_helm_charts.sh
@@ -23,27 +23,39 @@ configure_git() {
     git checkout "${branch_to_checkout}"
 }
 
+handle_legacy_cart() {
+  set +e
+  LEGACY_NEW_VERSION=$(buildkite-agent meta-data get "k8s-watcher-version")
+  if [ $? -eq 0 ]; then
+    set -e
+    sed -i -e "s/version.*/version: $LEGACY_NEW_VERSION/g" charts/k8s-watcher/Chart.yaml
+    make generate-kube
+    git add charts/k8s-watcher
+
+    git status
+    git commit -m "[skip ci] update generated manifests"
+    if [ $? -eq 0 ]; then
+      # git tag "komodor-agent-${NEW_VERSION}"
+      git tag "k8s-watcher-${LEGACY_NEW_VERSION}"
+    else
+      echo "Already up-to-date"
+    fi
+
+    git push -f && git push --tags || echo "Nothing to push!"
+  else
+    set -e
+    echo "Skipping legacy chart"
+  fi
+}
+
 configure_git
 
-LEGACY_NEW_VERSION=$(buildkite-agent meta-data get "k8s-watcher-version")
+handle_legacy_cart
+
 NEW_VERSION=$(buildkite-agent meta-data get "komodor-agent-version")
 APP_VERSION=$(buildkite-agent meta-data get "agent-version")
 
-sed -i -e "s/version.*/version: $LEGACY_NEW_VERSION/g" charts/k8s-watcher/Chart.yaml
+# Chance the version of the komodor-agent chart *locally* and push it to the repo (not pushing this to the remote!!!)
 sed -i -e "s/version.*/version: $NEW_VERSION/g" charts/komodor-agent/Chart.yaml
-make generate-kube
 
-git add charts/k8s-watcher
-# git add charts/komodor-agent
-
-git status
-git commit -m "[skip ci] update generated manifests"
-if [ $? -eq 0 ]; then
-  # git tag "komodor-agent-${NEW_VERSION}"
-  git tag "k8s-watcher-${LEGACY_NEW_VERSION}"
-else
-  echo "Already up-to-date"
-fi
-
-git push -f && git push --tags || echo "Nothing to push!"
 GITHUB_PAGES_REPO=${GITHUB_PAGES_REPO} ./publish.sh
