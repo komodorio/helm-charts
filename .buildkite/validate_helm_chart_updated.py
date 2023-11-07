2a24bb899ffa9515edacf7d026184db0626b0a21
Moran <123294681+Moran-k@users.noreply.github.com>
2023-11-07 14:24:54 +0200

    CU-8679415e9 - update validate_helm_chart_updated.py to support RC versions (#269)
    * update validate_helm_chart_updated.py to support RC versions
    
    * Fix grep
    
    ---------
    
    Co-authored-by: buildkite <buildkite@users.noreply.github.com>

--- .buildkite/validate_helm_chart_updated.py
+++ .buildkite/validate_helm_chart_updated.py
@@ -1,23 +1,46 @@
 import sys
-from command import cmd
-from time import sleep
-chart = sys.argv[1]
-version_updated = False
-new_version = cmd(f'buildkite-agent meta-data get "{chart}-version"', True)
+import subprocess
+import time
 
-cmd("helm repo add komodorio https://helm-charts.komodor.io")
-cmd("helm repo list")
+def run_cmd(command, expect_output=False):
+    result = subprocess.run(command, shell=True, text=True, capture_output=True)
+    if result.returncode != 0:
+        raise Exception(f"Command failed: {command}\n{result.stderr}")
+    return result.stdout.strip() if expect_output else ""
 
-for x in range(1, 10):
-    cmd("helm repo update", True)
-    current_version = cmd(f"helm show all komodorio/{chart} | grep 'version:' | cut -d ' ' -f 2", True)
-    if current_version == new_version:
-        print(f"Chart updated, current version is {current_version}, expected version is {new_version}")
-        version_updated = True
-        break
-    print(f"Waiting for chart to be updated before checking, current version is {current_version}, expected version is {new_version}")
-    sleep(10)
+def get_current_version(chart, new_version, is_rc):
+    if is_rc:
+        search_command = f"helm search repo komodorio/{chart} --versions | grep '{new_version}\s' | awk '{{ print $2 }}'"
+    else:
+        search_command = f"helm show chart komodorio/{chart} | grep 'version:' | cut -d ' ' -f 2"
+    return run_cmd(search_command, expect_output=True)
 
-if not version_updated:
-    print(f"chart '{chart}' not updated, current version is {current_version}, expected version is {new_version}")
-    exit(1)
+
+def main(chart, new_version):
+    run_cmd("helm repo add komodorio https://helm-charts.komodor.io")
+    run_cmd("helm repo update")
+
+    is_rc = "+RC" in new_version.upper()
+    version_updated = False
+
+    for _ in range(10):
+        current_version = get_current_version(chart, new_version, is_rc)
+        if current_version == new_version:
+            print(f"Chart updated, version {new_version} is available")
+            version_updated = True
+            break
+        print(f"Waiting for chart to be updated, expected version is {new_version}, current version is {current_version}")
+        time.sleep(10)
+
+    if not version_updated:
+        print(f"Chart '{chart}' not updated, current version is {current_version}, expected version is {new_version}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: script.py <chart>")
+        sys.exit(1)
+    chart = sys.argv[1]
+    new_version = run_cmd(f'buildkite-agent meta-data get "{chart}-version"', True)
+    main(chart, new_version)
