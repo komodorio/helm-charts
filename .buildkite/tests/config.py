03600ceb5588891051c0a6818d95bf34a497f57c
Moran <123294681+Moran-k@users.noreply.github.com>
2024-12-23 10:31:15 +0200

    CU-86c1djzn9 - fix(ci): Update tests after removing installationId authentication type from BE endpoints  (#345)
    Co-authored-by: buildkite <buildkite@users.noreply.github.com>

--- .buildkite/tests/config.py
+++ .buildkite/tests/config.py
@@ -3,6 +3,7 @@ import base64
 
 API_KEY = os.environ.get("API_KEY", "512ecd36-aa72-4d37-af48-8ecdc817c822")
 API_KEY_B64 = base64.b64encode(API_KEY.encode()).decode()
+PUBLIC_API_KEY = os.environ.get("PUBLIC_API_KEY", API_KEY)
 RELEASE_NAME = os.environ.get("RELEASE_NAME", "helm-test")
 CHART_PATH = os.environ.get("CHART_PATH", "../../charts/komodor-agent")
 VALUES_FILE_PATH = os.environ.get("VALUES_FILE_PATH", "")
