58bc9ed86f88cb05289b887c4dca43d3974d378a
nirbenator <nir.benatar@komodor.io>
2025-06-29 09:51:22 +0300

    chore: disable installation of crd in stabillity job

--- .buildkite/release_checks/scenarios/komodor_agent/scenario.py
+++ .buildkite/release_checks/scenarios/komodor_agent/scenario.py
@@ -12,26 +12,22 @@ class KomodorAgentScenario(Scenario):
     def __init__(self, kubeconfig):
         super().__init__("komodor-agent", kubeconfig)
         rc_chart_version = "--version " + os.getenv("CHART_VERSION")
-        self.agents = [{"clusterName": CLUSTER_NAME, "agentVersion": rc_chart_version, "name": "komodor-agent-rc"},
-                       {"clusterName": "komodor-agent-ga", "agentVersion": "", "name": "komodor-agent-ga"}]
+        self.agents = [{"clusterName": CLUSTER_NAME, "agentVersion": rc_chart_version, "name": "komodor-agent-rc", "extra_args": ""},
+                       {"clusterName": "komodor-agent-ga", "agentVersion": "", "name": "komodor-agent-ga", "extra_args": "--set capabilities.events.create=false"}]
 
-    def generate_installation_cmd(self, cluster_name, chart_version, name):
+    def generate_installation_cmd(self, cluster_name, chart_version, name, extra_args):
         return (f"helm repo add komodorio https://helm-charts.komodor.io && "
                 f"helm repo update && "
                 f"{self.helm} upgrade --install {name} komodorio/komodor-agent "
                 f"--set apiKey={API} "
                 f"--set clusterName={cluster_name} "
+                f"{extra_args} "
                 f"{chart_version} "
                 f"--namespace {name} --create-namespace")
 
-    async def install_komodor_agent(self, cluster_name, chart_version, name):
-        self.log(f"Starting to deploy {name}")
-        install_cmd = self.generate_installation_cmd(cluster_name, chart_version, name)
-
-        output, exit_code = await self.cmd(install_cmd, silent_errors=True)
-        if exit_code != 0:
-            self.error(f"Failed to deploy:\n\tCMD: {install_cmd}\n\tOutput: {output}")
-            raise Exception(f"Failed to deploy: {self.name}")
+    async def install_komodor_agent(self, cluster_name, chart_version, name, extra_args):
+        cmd = self.generate_installation_cmd(cluster_name, chart_version, name, extra_args)
+        await self.cmd(cmd)
 
     async def run(self):
         await asyncio.sleep(120)  # Wait x seconds before deploying, to let other deployments to finish
@@ -41,7 +37,8 @@ class KomodorAgentScenario(Scenario):
         for agent in self.agents:
             installation_tasks.append(asyncio.create_task(self.install_komodor_agent(agent["clusterName"],
                                                                                      agent["agentVersion"],
-                                                                                     agent["name"])))
+                                                                                     agent["name"],
+                                                                                     agent["extra_args"])))
 
         await asyncio.gather(*installation_tasks)
         self.log("Finished deploying")
