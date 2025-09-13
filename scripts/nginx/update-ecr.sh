268ace7820b5732f6f886dd18c90c555637505f9
buildkite <buildkite@users.noreply.github.com>
2025-09-13 20:33:10 +0300

    leftovers

--- scripts/nginx/update-ecr.sh
+++ scripts/nginx/update-ecr.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# check if regctl is installed
+if ! command -v regctl &> /dev/null; then
+    echo "regctl could not be found, please install it: https://regclient.org/install"
+    exit 1
+fi
+
 # Authenticate with AWS ECR & Docker Hub
 komo ci docker-login
 
 NGINX_VERSION="1.27.5"
 
 regctl image copy nginx:${NGINX_VERSION} public.ecr.aws/komodor-public/nginx:${NGINX_VERSION}
+regctl image copy nginx:${NGINX_VERSION} komodorio/nginx:${NGINX_VERSION}
